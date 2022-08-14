84806c2368667e0eeea0c0e06f8d386a1645cbba
Gaby Bonini <gabriellaine.bonini@al.infnet.edu.br>
2022-08-14 16:02:33 -0300

    Resolucao questao 10

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-# questao10-tp1
+# 
+
+        Este repositório contém exercícios que estão sob avaliação, referentes ao TP'S (1, 3)
+        e Assessment de Frameworks Vue e React.
+- [x] [Teste Prático 1]
+
+- [ ] Teste Prático 3
+
+- [ ] Assessment
+## Questão 10 - TP1
+
+Crie um novo projeto Web Vue.js com os comandos aprendidos e implemente as seguintes funcionalidades:
+
+Crie uma variável do tipo Array com o objeto que será uma tarefa, que vai possuir as propriedades: id (Number), descricao (String), estaCompleta (Boolean);
+Preencha esse array com várias tarefas e as propriedades acima;
+Exiba uma lista com essas tarefas exibindo a descrição;
+Implemente um elemento de interface para a tarefa que possuir a variável estaCompleta como True. Sugestões de elemento de interface: Texto sublinhado, Texto riscado, Cor diferente, etc;
+
+![img-min.png](img-min.png)
+
+### Resolução: [Questão 10]
+### Aluna: Gabriellaine Araujo Bonini - Noite - 22.3T
+
+******
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2>STATUS DAS TAREFAS DO DIA</h2>
+  </div>
+  <div class="container-sm">
+    <table class="table table-striped">
+      <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">DESCRIÇÃO</th>
+        <th scope="col">TAREFA</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr class="div" v-bind:key="tarefa.id" v-for="tarefa in tarefas">
+        <td>{{ tarefa.id }}</td>
+        <td>{{ tarefa.descricao }}</td>
+        <td v-if="tarefa.estaCompleta" id="completo">
+          <button type="button" class="btn btn-success">CONCLUÍDA</button></td>
+        <td v-else id="incompleto">
+          <button type="button" class="btn btn-danger">INCOMPLETA</button></td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+  },
+  data(){
+    return {
+      tarefas: [
+        {id: 1, descricao: "Revisar apresentação do TCC", estaCompleta: true},
+        {id: 2, descricao: "Abastecer o carro", estaCompleta: true},
+        {id: 3, descricao: "Revisar 50 Anotações do estágio", estaCompleta: true},
+        {id: 4, descricao: "Finalizar atividades da faculdade", estaCompleta: false},
+        {id: 5, descricao: "Ir ao banco", estaCompleta: false},
+        {id: 6, descricao: "Estudar por 1h", estaCompleta: true},
+        {id: 7, descricao: "Terminar Trabalho de casa antes das 18h", estaCompleta: false},
+        {id: 8, descricao: "Finalizar bootcamp", estaCompleta: true},
+        {id: 9, descricao: "Organizar Emails", estaCompleta: false},
+        {id: 10, descricao: "Yoga/Academia", estaCompleta: false}
+      ]
+    }
+  },
 }
 </script>
 
@@ -21,6 +57,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 50px;
 }
+
 </style>
